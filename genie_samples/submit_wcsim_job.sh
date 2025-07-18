b6a048e2effea06bee0c3de3663f9fea5e367f0c
Steven Doran <78985334+S81D@users.noreply.github.com>
2025-07-18 08:38:19 -0500

    Update submit_wcsim_job.sh
    World events grid resource update based on testing. 2hr, 3GB disk was leading to too many held jobs at certain sites.

--- genie_samples/submit_wcsim_job.sh
+++ genie_samples/submit_wcsim_job.sh
@@ -1,6 +1,6 @@
 RUN=$1
 
-# WORLD samples only run ~600 events per 20k GENIE file aka need low grid resources 
+# WORLD samples only run ~600 events per 20k GENIE file aka need low grid resources (recommended: 2GB mem, 4h walltime, 5GB disk)
 
 export INPUT_PATH=/pnfs/annie/scratch/users/doran/genie_wcsim_grid/  
 export GENIE=/pnfs/annie/persistent/simulations/genie3/G1810a0211a/standardv1.0/world/
@@ -14,4 +14,4 @@ OUTPUT_FOLDER=/pnfs/annie/scratch/users/doran/output/genie_wcsim
 mkdir -p $OUTPUT_FOLDER                                                 
 
 # wrapper script to submit your grid job
-jobsub_submit --memory=2000MB --expected-lifetime=2h -G annie --disk=3GB --resource-provides=usage_model=OFFSITE --blacklist=Omaha,Swan,Wisconsin,SU-ITS,RAL -f ${INPUT_PATH}/WCSim.tar.gz -f ${INPUT_PATH}/wcsim_container.sh -f ${DIRT}/annie_tank_flux.${RUN}.root -f ${GENIE}/gntp.${RUN}.ghep.root -d OUTPUT $OUTPUT_FOLDER file://${INPUT_PATH}/run_job.sh ${RUN}
+jobsub_submit --memory=2000MB --expected-lifetime=4h -G annie --disk=5GB --resource-provides=usage_model=OFFSITE --blacklist=Omaha,Swan,Wisconsin,SU-ITS,RAL -f ${INPUT_PATH}/WCSim.tar.gz -f ${INPUT_PATH}/wcsim_container.sh -f ${DIRT}/annie_tank_flux.${RUN}.root -f ${GENIE}/gntp.${RUN}.ghep.root -d OUTPUT $OUTPUT_FOLDER file://${INPUT_PATH}/run_job.sh ${RUN}
